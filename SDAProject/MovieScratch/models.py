3821c10412619d3e383a55d5e30cbfeb49d8bb12
keviiner <112650170+keviiner@users.noreply.github.com>
2023-10-08 13:12:16 +0300

    Revert "html"

--- SDAProject/MovieScratch/models.py
+++ SDAProject/MovieScratch/models.py
@@ -2,15 +2,14 @@ from django.db import models
 
 class Director(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    birth_date = models.DateField()
-    directors = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='directors_worked_with')
+    date = models.DateField()
 
     def __str__(self):
         return self.name
 
 class Actor(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    birth_date = models.DateField()
+    date = models.DateField()  # Corrected 'Datefield' to 'DateField'
 
     def __str__(self):
         return self.name
@@ -21,7 +20,7 @@ class Movie(models.Model):
     genres = models.CharField(max_length=25)
     description = models.CharField(max_length=500, null=True)
     rating = models.FloatField(blank=True, null=True)
-    directors = models.ManyToManyField(Director, related_name='movies_directed', blank=True)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True, blank=True, related_name='movies_directed')
     actors = models.ManyToManyField(Actor, related_name='movies_acted_in', blank=True)
 
     def __str__(self):
