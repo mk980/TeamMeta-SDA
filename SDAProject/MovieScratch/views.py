fd081bc3a6056003bd5506a8ef8325ec11b6dd76
victorcid <vcfiguera@gmail.com>
2023-10-21 11:06:45 +0300

    Basic functionality

--- SDAProject/MovieScratch/views.py
+++ SDAProject/MovieScratch/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Movie
+
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, "index.html", {"movies": movies})
+
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, "movie_detail.html", {"movie": movie})
